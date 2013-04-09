b411ff23012d13a84225046fd3de3bb67e8af5d5
Jason Oster <jay@pubnub.com>
2013-04-09 11:18:01 -0700

    Initial commit

---
+++ public/js/resources.js
@@ -0,0 +1,24 @@
+game.resources = {
+    /* Graphics. */
+    "img" : [
+    ],
+
+    /* Maps. */
+    "map" : [
+    ],
+
+    /* Sound effects. */
+    "sfx" : [
+        "blip1",
+        "blip2",
+        "blip3",
+        "blip4",
+        "blip5",
+        "blip6",
+        "blip7"
+    ],
+
+    /* Background music. */
+    "bgm" : [
+    ]
+};
